027be5651f5143f947ac19a5e66183209e2b906f
Tai Ho <hotai1806@gmail.com>
2025-11-25 15:48:27 +0700

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [dashboardUid, setDashboardUid] = useState(null);
 
   useEffect(() => {
-    fetch("/grafana/api/search?type=dash-db")
+    fetch("/api/search?type=dash-db")
       .then((res) => res.json())
       .then((data) => {
         if (data && data.length > 0) {
@@ -22,7 +22,7 @@ function App() {
       {/* grafana/d-solo/null?orgId=1&from=1763684129792&to=1763705729792&timezone=browser&panelId=panel-4&__feature.dashboardSceneSolo=true */}
       {dashboardUid && (
         <iframe
-          src={`/grafana/d/22e81737-1033-4690-b031-bc0896ef8af3/test-dashboard?orgId=1&kiosk`}
+          src={`/d/22e81737-1033-4690-b031-bc0896ef8af3/test-dashboard?orgId=1&kiosk`}
           style={{ width: "100%", height: "100%", border: "none" }}
           title="Grafana Dashboard"
         />
